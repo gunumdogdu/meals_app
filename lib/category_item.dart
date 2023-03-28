48e133868f57a9020aa5954892d57f4257dfa509
gunumdogdu <emirgunumdogdu@gmail.com>
2023-03-28 22:13:32 +0300

    assests,models,files,folders,general look

---
+++ lib/category_item.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/category_meals_screen.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final Color color;
+
+  CategoryItem(this.id, this.title, this.color);
+  void selectCategory(BuildContext ctx) {
+    Navigator.of(ctx).push(MaterialPageRoute(
+      builder: (_) {
+        return CategoryMealsScreen(id, title);
+      },
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      //Gesture detector with a ripple effect, InkWell
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).colorScheme.primary,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        child: Text(title),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.7),
+              color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(16),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'categories_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,31 +7,59 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner:
+          false, //REMOVED DEBUG BANNER FOR AEST. PURPOSES
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        canvasColor: Colors.red.shade50,
+        fontFamily: 'Raleway',
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.amber,
+        ).copyWith(
+          secondary: Colors.deepPurple,
+          surface: Colors.grey,
+          error: Colors.red,
+        ),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodySmall: TextStyle(
+                color: Colors.grey,
+                fontSize: 14,
+              ),
+              bodyMedium: TextStyle(
+                  color: Color.fromRGBO(250, 249, 246, 1),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                  fontFamily: 'RobotoCondensed'),
+              titleLarge: TextStyle(
+                color: Colors.deepPurple,
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+              displayLarge: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.deepPurple),
+            ),
+        appBarTheme: AppBarTheme(
+          titleSpacing: 0,
+          elevation: 0,
+          backgroundColor: Colors.amber,
+          iconTheme: IconThemeData(
+            color: Colors.red,
+          ),
+          actionsIconTheme: IconThemeData(
+            color: Colors.amber,
+          ),
+          //DID NOT UNDERSTAND
+          titleTextStyle: TextStyle(
+              color: Colors.deepPurple,
+              fontFamily: 'Raleway',
+              fontSize: 24,
+              fontWeight: FontWeight.bold),
+          centerTitle: true,
+        ),
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-    @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('DeliMeals'),
-      ),
-      body: Center(
-        child: Text('Navigation Time!'),
-      ),
+      home: CategoriesScreen(),
     );
   }
 }
