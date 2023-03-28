874c4aa8960eb3b8505ab0b0a1d5610cc41de35b
gunumdogdu <emirgunumdogdu@gmail.com>
2023-03-29 00:00:34 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/category_meals_screen.dart';
 import 'categories_screen.dart';
 
 void main() => runApp(MyApp());
@@ -59,7 +60,12 @@ class MyApp extends StatelessWidget {
           centerTitle: true,
         ),
       ),
-      home: CategoriesScreen(),
+
+      initialRoute: '/',
+      routes: {
+        '/': (ctx) => CategoriesScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+      },
     );
   }
 }
